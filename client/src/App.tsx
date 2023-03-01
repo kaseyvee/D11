3e4d9aae74035404330c091f1b29a4d4076f96ed
kethnal <kaseyvaldez@hotmail.com>
2023-02-28 20:47:15 -0800

    Start setting up contentful

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,18 @@
 import LandingPage from "./pages/LandingPage";
+import { ContentfulClient, ContentfulProvider } from 'react-contentful';
+
+const contentfulClient = new ContentfulClient({
+  accessToken: "003LiaQKZbPsUB9otZEjfXNE1q2NSbu4pp8SUiQKUAE",
+  space: "v9b7yte4wy9l",
+});
 
 function App() {
   return (
-    <div className="App">
-      <LandingPage />
-    </div>
+    <ContentfulProvider client={contentfulClient}>
+      <div className="App">
+        <LandingPage />
+      </div>
+    </ContentfulProvider>
   );
 }
 
