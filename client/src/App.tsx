515633afd2a3ed471a790d73fd2ef58735e6b818
kethnal <kaseyvaldez@hotmail.com>
2023-03-07 14:52:25 -0800

    Attempt contentful implementation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
 import LandingPage from "./pages/LandingPage";
 import { ContentfulClient, ContentfulProvider } from 'react-contentful';
+import React from "react";
 
+// @ts-ignore
 const contentfulClient = new ContentfulClient({
   accessToken: "003LiaQKZbPsUB9otZEjfXNE1q2NSbu4pp8SUiQKUAE",
   space: "v9b7yte4wy9l",
