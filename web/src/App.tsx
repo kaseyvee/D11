dc7214e6f80042033ba5b53e50262d390da63d42
wagabooga <mattyhutwo@gmail.com>
2023-03-13 01:08:18 -0700

    Reorganize navbar/footer and fix line-spacing

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import NavBar from "./components/Navbar";
 import LandingPage from "./pages/LandingPage";
 import MenuPage from "./pages/MenuPage";
-import SamplePage2 from "./pages/SamplePage2";
 
 import "./styles/index.scss";
 
@@ -12,9 +13,11 @@ const App: React.FC = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage/>}/>
-        <Route path="/2" element={<SamplePage2/>}/>
-        <Route path="/menu" element={<MenuPage/>}/>
+        {/* <Route path="/menu" element={<MenuPage/>}/> */}
       </Routes>
+
+      <NavBar/>
+      {/* <Footer/> */}
     </BrowserRouter>
   );
 }
