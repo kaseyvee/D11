28e7593feb4b063233b542992ba24f0f336cf5b9
wagabooga <mattyhutwo@gmail.com>
2023-03-14 03:10:31 -0700

    safety

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,22 +1,39 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Footer from "./components/Footer";
-import NavBar from "./components/Navbar";
-import Homepage from "./pages/Homepage";
 
-import "./styles/index.scss";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import {useContentful} from "react-contentful";
+
+// space: 'juzkia2ppex9',
+// accessToken: 'yma-kCPzR_Pd8X6Bv_ZHECeETLG3X4UyCHi3LeTxFAI',
+
+
+
+// const {data, fetched} = useContentful({
+//   contentType: 'blogPost',
+//   query: {
+//       "order": "-sys.createdAt",
+//   },
+// });
+
+
+
+
 
 const App: React.FC = () => {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Homepage/>}/>
-        {/* <Route path="/menu" element={<MenuPage/>}/> */}
-      </Routes>
+    <React.Fragment>
+        <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Homepage/>}/>
+              {/* <Route path="/menu" element={<MenuPage/>}/> */}
+            </Routes>
 
-      {/* <NavBar/> */}
-      {/* <Footer/> */}
-    </BrowserRouter>
+            {/* <NavBar/> */}
+            {/* <Footer/> */}
+          </BrowserRouter>
+    </React.Fragment>
   );
 }
 
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App'
+
+// // if any errors try https://reactrouter.com/en/main/start/tutorial
+// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+
 import App from './App'
 import "./styles/index.scss"
-// if any errors try https://reactrouter.com/en/main/start/tutorial
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(<App />);
