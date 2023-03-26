b72ba62ae98ece032f2f007911d872ff147adacc
wagabooga <mattyhutwo@gmail.com>
2023-03-26 00:19:13 -0700

    page height setup

--- web/src/components/homepage/HomepageHero.tsx
+++ web/src/components/homepage/HomepageHero.tsx
@@ -1,7 +1,6 @@
 const HomepageHero: React.FC = () => {
   return (
     <div className="homepage-hero">
-      <h1>hello asdasd</h1>
       <p>asdasdkjaskdjaskdj asdj kasjdka jsd</p>
       <button>asd</button>
       <button>asd</button>
