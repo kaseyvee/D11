d3afbe3c2918e451ad14c8b87e58f56a6a205eaa
kethnal <kaseyvaldez@hotmail.com>
2023-03-29 05:55:02 -0700

    Style footer with map placeholder

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
+import HeroButton from "./buttons/HeroButton";
 // does cody want to be able to change hours/contact?
 
 const Footer: React.FC = () => {
@@ -10,43 +9,65 @@ const Footer: React.FC = () => {
 
   return (
     <div className="footer">
-      <div className="foooter-top">
-        <div className="footer-top-info">
-          <div className="footer-top-info-item">
-            <h1>HOURS</h1>
-            <div>
+
+      <div className="footer_top">
+        <div className="footer_top_info">
+
+          <div className="footer_top_info_item">
+            <h1 className="footer_top_info_item_header">HOURS</h1>
+            <div className="footer_top_info_item_content">
               <p><strong>Fri & Sat:</strong> 5 pm - 2 pm</p>
               <p><strong>Tues, Wed, Thurs:</strong> 5 pm - 1 am</p>
               <p><strong>Sun:</strong> 5pm - 12am</p>
               <p><strong>Mon:</strong> Closed</p>
             </div>
           </div>
-          <div className="footer-top-info-item">
-            <h1>LOCATION</h1>
-            <div>
+
+          <div className="footer_top_info_item">
+            <h1 className="footer_top_info_item_header">LOCATION</h1>
+            <div className="footer_top_info_item_content">
               <p>4063 Main Street, Vancouver, BC</p>
             </div>
           </div>
-          <div className="footer-top-info-item">
-            <h1>CONTACT</h1>
-            <div>
+
+          <div className="footer_top_info_item">
+            <h1 className="footer_top_info_item_header">CONTACT</h1>
+            <div className="footer_top_info_item_content">
               <p>604.566.2021</p>
               <p>districteleven_@outlook.com</p>
             </div>
           </div>
+
         </div>
-        <div className="google-map">
-          google map goes here
-        </div>
+
+        <img
+          src="src/assets/map-example.png"
+          className="footer_top_map"
+        />
       </div>
-      <div className="footer-bottom">
+
+      <div className="footer_bottom">
         <div>© 2021 District Eleven</div>
-        <div>insert logo svg here</div>
-        <div className="social-medias">
-          <Link to={instagramLink}>Instagram</Link>
-          <Link to={tikTokLink}>TikTok</Link>
+        <img
+          src="src/assets/logo.svg"
+          className="footer_bottom_logo"
+        />
+        <div className="footer_bottom_socials">
+          <HeroButton
+            to={instagramLink}
+            color="yellow"
+            children="Instagram"
+            className="footer-button"
+          />
+          <HeroButton
+            to={tikTokLink}
+            color="yellow"
+            children="Tiktok"
+            className="footer-button"
+          />
         </div>
       </div>
+
     </div>
   );
 }
